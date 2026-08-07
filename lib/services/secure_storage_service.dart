fda9571617af038c672f4f95ae20c56005259991
lfnsahil1-sketch <lfnsahil1@gmail.com>
2026-08-07 16:14:57 +0300

    Create secure_storage_service.dart

---
+++ lib/services/secure_storage_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  static const _storage = FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+  );
+
+  static const String _keyGroqApi = 'groq_api_key';
+
+  static Future<void> saveApiKey(String apiKey) async {
+    await _storage.write(key: _keyGroqApi, value: apiKey);
+  }
+
+  static Future<String?> getApiKey() async {
+    return await _storage.read(key: _keyGroqApi);
+  }
+
+  static Future<void> deleteApiKey() async {
+    await _storage.delete(key: _keyGroqApi);
+  }
+}
